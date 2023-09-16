82283ace6275535e20196b26df7aabf549ee4fab
Hiato27 <84791904+Hiato27@users.noreply.github.com>
2023-09-16 17:13:08 -0300

    relleno index

---
+++ src/components/valores.jsx
@@ -0,0 +1,30 @@
+import '../css/valores.css'
+
+function Valores(){
+return(
+<>
+<div className="conte">
+<div className='vision'>
+<div className='imge'> <img src="/binocular.png" alt="" /></div>
+<div className='titu'><h1>VISIÓN</h1></div>
+<div className='tex'><p>Facilitar y promover la participación activa en el deporte para todas las edades en el Departamento de Soriano, a través de una plataforma digital accesible y eficiente. Nuestra misión es simplificar y agilizar los procesos de registro en actividades deportivas, fomentando un estilo de vida saludable y activo en nuestra comunidad.</p></div>
+</div>
+<div className='mision'>
+<div className='imge'><img src="/montaña.png" alt="" /></div>
+<div className='titu'><h1>MISIÓN</h1></div>
+<div className='tex'><p>Ser la plataforma de referencia en el Departamento de Soriano que inspire y facilite la práctica del deporte en todas las etapas de la vida. Buscamos convertirnos en un referente en la promoción de la actividad física, proporcionando herramientas innovadoras y recursos de calidad que impulsen la participación deportiva y contribuyan al bienestar de la comunidad.</p></div>
+</div>
+<dir className="valores">
+<div className='imge'><img src="/valores}.png" alt="" /></div>
+<div className='titu'><h1>VALORES</h1></div>
+<div className='tex'><p>En nuestra plataforma, valoramos la accesibilidad, eficiencia y salud, promoviendo un estilo de vida activo e inclusivo. Nos comprometemos con la comunidad y la innovación, manteniendo una comunicación transparente. Estos valores son fundamentales para nuestra misión de fomentar el deporte en Soriano.</p></div>
+</dir>
+
+</div>
+
+
+</>
+);
+}
+
+export default Valores;
