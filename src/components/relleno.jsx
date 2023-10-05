8b783a24420a7aa3e525c01f64d062fa8d93378c
eldiegon123 <107365577+eldiegon123@users.noreply.github.com>
2023-10-04 21:49:54 -0300

    Delete src/components/relleno.jsx

--- src/components/relleno.jsx
+++
@@ -1,9 +0,0 @@
-import '../css/relleno.css';
-import Eventos from './eventos';
-function Relleno(){
-    return(
-    <div className='relleno'>
-    <Eventos/>
-    </div>);
-}
-export default Relleno;
