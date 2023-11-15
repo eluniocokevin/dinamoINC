7a8e0e9c6852c1fab59d972149c647d89f804dec
Hiato27 <84791904+Hiato27@users.noreply.github.com>
2023-11-14 22:27:21 -0300

    Add files via upload

--- archivos2/usuarios/inscribirUsuario.php
+++ archivos2/usuarios/inscribirUsuario.php
@@ -28,36 +28,41 @@ if ($method === "POST") {
     $contrasena = $data["contrasena"];
     $ActividadId = $data["ActividadId"];
 
-    $sql = "SELECT * FROM usuario WHERE ci = ?";
-    $stmt = mysqli_prepare($conexion, $sql);
-    mysqli_stmt_bind_param($stmt, "s", $cedula);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
-
-    if ($result && mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_assoc($result);
-        if ($contrasena == $row["contrasena"] ) {
+    $sql_check_cedula = "SELECT cedula, deporteid FROM realiza WHERE cedula = ? AND deporteid = ?";
+    $stmt_check_cedula = mysqli_prepare($conexion, $sql_check_cedula);
+    mysqli_stmt_bind_param($stmt_check_cedula, "ss", $cedula, $ActividadId);
+    mysqli_stmt_execute($stmt_check_cedula);
+    $result_check_cedula = mysqli_stmt_get_result($stmt_check_cedula);
+    
+    if ($result_check_cedula && mysqli_num_rows($result_check_cedula) > 0) {
+        echo json_encode(array("error" => "Usted ya está registrado en este deporte"));
         
-            $sql1 = "INSERT INTO realiza(cedula, deporteid) VALUES (?, '$ActividadId')";
-            $stmt1 = mysqli_prepare($conexion, $sql1);
-            try {
-
-                mysqli_stmt_bind_param($stmt1, "s", $cedula);
-                mysqli_stmt_execute($stmt1);
-                echo json_encode(array("success" => "Registro exitoso"));
-                
-            } catch (Exception $e) {
-                echo json_encode(array("error" => $e->getMessage()));
-            }
-
-
-       
-
-        } else {
-            echo json_encode(array("error" => "Credenciales incorrectas"));
-        }
     } else {
-        echo json_encode(array("error" => "Usuario no encontrado"));
+
+        $sql = "SELECT * FROM usuario WHERE ci = ?";
+        $stmt = mysqli_prepare($conexion, $sql);
+        mysqli_stmt_bind_param($stmt, "s", $cedula);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+    
+        if ($result && mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            if ($contrasena == $row["contrasena"]) {
+                $sql1 = "INSERT INTO realiza(cedula, deporteid) VALUES (?, ?)";
+                $stmt1 = mysqli_prepare($conexion, $sql1);
+                try {
+                    mysqli_stmt_bind_param($stmt1, "ss", $cedula, $ActividadId);
+                    mysqli_stmt_execute($stmt1);
+                    echo json_encode(array("success" => "Registro exitoso"));
+                } catch (Exception $e) {
+                    echo json_encode(array("error" => $e->getMessage()));
+                }
+            } else {
+                echo json_encode(array("error" => "Credenciales incorrectas"));
+            }
+        } else {
+            echo json_encode(array("error" => "Usuario no encontrado"));
+        }
     }
 } else {
     echo json_encode(array("error" => "Método de solicitud inválido"));
